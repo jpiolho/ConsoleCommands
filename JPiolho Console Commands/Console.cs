815dc9df55bbed02e7cd229d15428e8a66b23827
JPiolho <jpiolhox@gmail.com>
2018-07-02 21:58:49 +0200

    Added console commands code

---
+++ JPiolho Console Commands/Console.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPiolho.ConsoleCommands
+{
+    public static class ConsoleCommands
+    {
+        private static bool stopRequested = false;
+
+        /// <summary>
+        /// Starts the console command subsystem.
+        /// The calling thread will be blocked by UI reading until the subsystem is stopped.
+        /// </summary>
+        public static void Initialize()
+        {
+            stopRequested = false;
+
+            while (!stopRequested)
+            {
+                var line = Console.ReadLine().Trim();
+
+                // Ignore if line is empty
+                if (line.Length == 0)
+                    continue;
+
+                var tokens = GetTokens(line);
+
+                // Ignore if there was nothing
+                if (tokens.Length == 0)
+                    continue;
+
+                string command = tokens[0].ToLower();
+                string[] arguments = new string[tokens.Length - 1];
+
+                // Copy arguments
+                if (tokens.Length > 1)
+                    Array.Copy(tokens, 1, arguments, 0, tokens.Length - 1);
+
+                OnCommand?.Invoke(command, arguments);
+
+                // Call registered command
+                if (registeredCommands.ContainsKey(command))
+                {
+                    registeredCommands[command]?.Invoke(arguments);
+                }
+                else // Or unknown command
+                {
+                    OnUnknownCommand?.Invoke(command, arguments);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Stop the console command subsystem
+        /// </summary>
+        public static void Stop()
+        {
+            stopRequested = true;
+        }
+
+
+
+        public delegate void OnRegisteredCommandCallbackHandler(string[] arguments);
+        private static Dictionary<string, OnRegisteredCommandCallbackHandler> registeredCommands = new Dictionary<string, OnRegisteredCommandCallbackHandler>();
+
+        /// <summary>
+        /// Register a command to a callback. This callback will be called when the command is entered.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="callback"></param>
+        public static void RegisterCommand(string command, OnRegisteredCommandCallbackHandler callback)
+        {
+            registeredCommands.Add(command, callback);
+        }
+
+
+        public delegate void OnCommandEventHandler(string command, string[] arguments);
+        public static event OnCommandEventHandler OnCommand;
+
+        public delegate void OnUnknownCommandEventHandler(string command, string[] arguments);
+        public static event OnCommandEventHandler OnUnknownCommand;
+
+
+
+
+
+        private static string[] GetTokens(string input)
+        {
+            List<string> tokens = new List<string>();
+
+
+            string s = "";
+            void AddToken()
+            {
+                if (s == "") return;
+                tokens.Add(s);
+                s = "";
+            }
+
+            bool escaping = false;
+            bool quote = false;
+            for (var i = 0; i < input.Length; i++)
+            {
+                var c = input[i];
+
+                if (escaping)
+                {
+                    s += c;
+                    escaping = false;
+                }
+                else if (c == '\\')
+                {
+                    escaping = true;
+                }
+                else if (c == '"')
+                {
+                    if (!quote)
+                    {
+                        quote = true;
+                    }
+                    else
+                    {
+                        quote = false;
+                        AddToken();
+                    }
+                }
+                else if (c == ' ')
+                {
+                    if (quote)
+                    {
+                        s += c;
+                    }
+                    else
+                    {
+                        AddToken();
+                    }
+                }
+                else
+                {
+                    s += input[i];
+                }
+
+            }
+
+            AddToken();
+
+            return tokens.ToArray();
+        }
+    }
+}
